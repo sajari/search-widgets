1481f067545f5b9ed49ea9c9bba8f8bddbf00d92
Thanh Pham <zlatan.pham@gmail.com>
2022-03-08 09:32:53 +0700

    chore: remove unvalued naming

--- src/utils/tests/ranges.test.ts
+++ src/utils/tests/ranges.test.ts
@@ -2,42 +2,36 @@ import { Range } from '@sajari/react-hooks';
 
 import { isRange, paramToRange, rangeToParam } from '../range';
 
-describe('isRange function should work correctly', () => {
-  test.each([
-    [undefined, false],
-    [null, false],
-    [12, false],
-    ['12', false],
-    ['abc', false],
-    [[], false],
-    [[1], false],
-    [['1'], false],
-    [['1', 1], false],
-    [[1, '1'], false],
-    [[1, '1', 2], false],
-    [[1, 2], true],
-    [[2, 1], true],
-  ])('isRange($s)', (input, expected) => {
-    expect(isRange(input)).toBe(expected);
-  });
+test.each([
+  [undefined, false],
+  [null, false],
+  [12, false],
+  ['12', false],
+  ['abc', false],
+  [[], false],
+  [[1], false],
+  [['1'], false],
+  [['1', 1], false],
+  [[1, '1'], false],
+  [[1, '1', 2], false],
+  [[1, 2], true],
+  [[2, 1], true],
+])('isRange($s)', (input, expected) => {
+  expect(isRange(input)).toBe(expected);
 });
 
-describe('rangeToParam function should work correctly', () => {
-  test.each<[Range, string]>([
-    [[1, 2], '1:2'],
-    [[3, 1], '3:1'],
-    [[4, 5], '4:5'],
-  ])('rangeToParam($o)', (input, expectedOutput) => {
-    expect(rangeToParam(input)).toBe(expectedOutput);
-  });
+test.each<[Range, string]>([
+  [[1, 2], '1:2'],
+  [[3, 1], '3:1'],
+  [[4, 5], '4:5'],
+])('rangeToParam($o)', (input, expectedOutput) => {
+  expect(rangeToParam(input)).toBe(expectedOutput);
 });
 
-describe('paramToRange function should works correctly', () => {
-  test.each([
-    ['1:2', [1, 2]],
-    ['3:5', [3, 5]],
-    ['99:42', [99, 42]],
-  ])('paramToRange($o)', (input, expectedOutput) => {
-    expect(paramToRange(input)).toStrictEqual(expectedOutput);
-  });
+test.each([
+  ['1:2', [1, 2]],
+  ['3:5', [3, 5]],
+  ['99:42', [99, 42]],
+])('paramToRange($o)', (input, expectedOutput) => {
+  expect(paramToRange(input)).toStrictEqual(expectedOutput);
 });
