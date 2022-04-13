f68395fa8eb8adbbd09646ccd8fa18ce219c5402
Tuan Dao <heryhudson@gmail.com>
2022-04-13 23:17:34 +0700

    resolve comments

--- src/tracking.tsx
+++ src/tracking.tsx
@@ -1,5 +1,6 @@
 import { EventTracking, Pipeline, Tracking, useTracking, Variables } from '@sajari/react-hooks';
-import { SearchProvider } from '@sajari/react-search-ui';
+import { isNullOrUndefined } from '@sajari/react-sdk-utils';
+import { Results, SearchProvider } from '@sajari/react-search-ui';
 import { useEffect, useMemo } from 'react';
 
 import { TrackingProps } from './types';
@@ -18,16 +19,13 @@ function TrackingWidget({ events }: { events: TrackingProps['events'] }) {
         if (e.event === 'load') {
           const { event, id, data } = e.metadata;
           tracking.searchIOAnalytics.track(event, id, data);
-        } else if (e.selector) {
-          document.querySelectorAll(e.selector).forEach((element) => {
-            element.addEventListener(e.event, (nativeEvent) => {
-              nativeEvent.preventDefault();
-              nativeEvent.stopPropagation();
-
+        } else if (!isNullOrUndefined(e.selector)) {
+          document.body.addEventListener(e.event, (nativeEvent) => {
+            const element = nativeEvent.target as Element;
+            if (element.matches(e.selector as string)) {
               const { event, id, data } = e.metadata;
-
               tracking.searchIOAnalytics.track(event, id, data);
-            });
+            }
           });
         }
       });
@@ -60,13 +58,7 @@ export default (props: TrackingProps) => {
 
   return (
     <SearchProvider searchOnLoad search={searchContext}>
-      {['development', 'test'].includes(process.env.NODE_ENV ?? '') && (
-        <article>
-          <h3>
-            <a href="https://example.com">Ally Ring</a>
-          </h3>
-        </article>
-      )}
+      <Results />
       <TrackingWidget events={events} />
     </SearchProvider>
   );
