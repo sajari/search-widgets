84eab2fd3e8839f7a9e2f7835874a42b2d5b076f
Nick <33882702+chidojiro@users.noreply.github.com>
2022-04-26 13:52:11 +0700

    test: add search input and overlay e2e tests (#266)
    * feat: search input tests
    
    * chore: update changeset
    
    * chore: remove changeset
    
    * feat: search overlay tests (#267)
    
    * feat: search overlay tests
    
    * chore: remove changeset

--- cypress/integration/input.test.ts
+++ cypress/integration/input.test.ts
@@ -1,12 +1,63 @@
+import 'cypress-localstorage-commands';
+
+const defaultConfigs = {
+  account: '1603163345448404241',
+  collection: 'sajari-test-fashion2',
+  pipeline: 'query',
+  preset: 'shopify',
+  mode: 'typeahead',
+  redirect: {
+    url: '',
+    queryParamName: 'q',
+  },
+};
+
+const visitSearchInput = (configs = defaultConfigs) => {
+  cy.setLocalStorage('code-content-search-input', JSON.stringify(configs));
+  cy.setLocalStorage('active-widget', 'search-input');
+  cy.visit('/');
+
+  cy.get('#preview').find('[type="search"]').first().should('be.visible');
+};
+
 describe('Search Input', async () => {
   it('Should redirect after running a search', () => {
-    cy.visit('/');
-    cy.get('#toolbar button').click();
-    // eslint-disable-next-line
-    cy.get('[role="listbox"] [role="option"]:nth-child(3)').click().wait(3000);
+    visitSearchInput();
 
-    cy.get('#preview').find('[type="search"]').first().should('be.visible').type('shirt');
+    cy.get('[type="search"]').first().type('shirt');
+    cy.get('form').submit();
+    cy.url().should('include', '?q=shirt');
+  });
 
+  it('Should show suggestions on suggestions mode', () => {
+    cy.intercept('POST', '**/Search', { fixture: 'autocomplete' });
+
+    visitSearchInput({ ...defaultConfigs, mode: 'suggestions' });
+
+    cy.get('[type="search"]').first().type('shi');
+
+    cy.get('h6').contains('Suggestions').should('be.visible');
+  });
+
+  it('Should show typeahead on typeahead mode', () => {
+    cy.intercept('POST', '**/Search', { fixture: 'autocomplete' });
+    visitSearchInput({ ...defaultConfigs, mode: 'typeahead' });
+
+    cy.get('[type="search"]').first().type('shi');
+    cy.get('[type="search"]').first().prev().should('have.text', 'shirt');
+  });
+
+  it('Should redirect to /search when redirect.url is "search"', () => {
+    visitSearchInput({
+      ...defaultConfigs,
+      mode: 'typeahead',
+      redirect: {
+        url: 'search',
+        queryParamName: 'q',
+      },
+    });
+
+    cy.get('[type="search"]').first().type('shirt');
     cy.get('form').submit();
     cy.url().should('include', '/search?q=shirt');
   });
--- cypress/integration/overlay.test.ts
+++ cypress/integration/overlay.test.ts
@@ -1,29 +1,53 @@
-describe('Search Overlay', async () => {
+const defaultConfigs = {
+  account: '1603163345448404241',
+  collection: 'sajari-test-fashion2',
+  pipeline: 'query',
+  preset: 'shopify',
+  filters: [
+    {
+      name: 'vendor',
+      field: 'vendor',
+      title: 'Vendor',
+      searchable: true,
+    },
+  ],
+  options: {
+    mode: 'overlay',
+    buttonSelector: '#button',
+    inputSelector: '#search-input',
+  },
+};
+
+const visitSearchOverlay = (configs = defaultConfigs) => {
+  cy.setLocalStorage('code-content-search-input', JSON.stringify(configs));
+  cy.setLocalStorage('active-widget', 'overlay');
+  cy.visit('/');
+};
+
+beforeEach(() => {
+  cy.intercept('POST', '**/Search', { fixture: 'search-overlay' });
+  visitSearchOverlay();
+});
+
+describe('Search Overlay', () => {
   it('Should open modal and run search successfully', () => {
-    cy.visit('/');
-    cy.get('#toolbar button').click();
-    // eslint-disable-next-line
-    cy.get('[role="listbox"] [role="option"]:last-child').click().wait(3000);
-
-    cy.get('#preview')
-      .find('#button')
-      .should('be.visible')
-      .then((e) => Cypress.$(e).trigger('click'));
-
+    cy.get('#preview').find('#button').should('be.visible').click();
     cy.get('[data-testid="modal"]').find('[type="search"]').should('be.visible').type('shirt');
-
     cy.get('[data-testid="options-bar"] strong').should('have.text', 'shirt');
     cy.get('button[aria-label="Close"]').click();
     cy.get('[data-testid="modal"]').should('not.exist');
   });
 
   it('Should use the value from "inputSelector" element to search', () => {
-    cy.visit('/');
-    cy.get('#toolbar button').click();
-    cy.get('[role="listbox"] [role="option"]:last-child').click();
-
     cy.get('#preview').find('#search-input').type('jacket');
     cy.get('#button').click();
     cy.get('[type="search"]').should('have.value', 'jacket');
+  });
+
+  it('Should redirect to product page on product click', () => {
+    cy.get('#preview').find('#search-input').type('shirt');
+    cy.get('#button').click();
+    cy.get('[data-testid="overlay-results"]').find('a').first().click();
+    cy.url().should('include', '/products/ultime-shirt-dress-2');
   });
 });
