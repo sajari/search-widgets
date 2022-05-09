689b2b5554132efba52a6c45c7f0196565ebc010
Tuan Dao <heryhudson@gmail.com>
2022-05-09 15:10:02 +0700

    update aria-label

--- src/interface/Options/ToggleFilters.tsx
+++ src/interface/Options/ToggleFilters.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@sajari/react-components';
+import { Button, Label } from '@sajari/react-components';
 import { useTranslation } from 'react-i18next';
 import tw from 'twin.macro';
 
@@ -8,18 +8,22 @@ export default () => {
   const { filtersShown, setFiltersShown } = useInterfaceContext();
   const toggleFilters = () => setFiltersShown(!filtersShown);
   const { t } = useTranslation('filter');
+  const label = filtersShown ? t('hide') : t('show');
 
   return (
     <Button
-      role="checkbox"
+      role="switch"
       aria-checked={filtersShown}
-      aria-label="Toggle Filters"
       type="button"
       size="sm"
       onClick={toggleFilters}
       css={tw`whitespace-nowrap m-0`}
+      id="toggle-filters"
     >
-      {filtersShown ? t('hide') : t('show')}
+      {label}
+      <Label css={tw`sr-only`} htmlFor="toggle-filters">
+        Toggle Filters
+      </Label>
     </Button>
   );
 };
