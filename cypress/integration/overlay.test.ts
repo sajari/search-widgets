c8d36a77962644aa7b888a83c189ae7931598f27
Thanh Pham <zlatan.pham@gmail.com>
2022-06-24 08:52:35 +0700

    test: add test to check URL sync should not be in overlay mode

--- cypress/integration/overlay.test.ts
+++ cypress/integration/overlay.test.ts
@@ -50,4 +50,12 @@ describe('Search Overlay', () => {
     cy.get('[data-testid="overlay-results"]').find('a').first().click();
     cy.url().should('include', '/products/ultime-shirt-dress-2');
   });
+
+  it('Should not inject URL param after changing UI state', () => {
+    cy.get('#button').click();
+    cy.get('[data-testid="modal"]').find('[type="search"]').should('be.visible').type('jacket');
+    // eslint-disable-next-line
+    cy.wait(500);
+    cy.url().should('not.include', 'q=jacket');
+  });
 });
