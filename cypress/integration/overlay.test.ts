4337d7574ae0c6ed9aa83406b0e32dc36516df1a
Thanh Pham <zlatan.pham@gmail.com>
2022-06-24 10:39:01 +0700

    chore: add reason for the need of eslint-disabled-line

--- cypress/integration/overlay.test.ts
+++ cypress/integration/overlay.test.ts
@@ -54,6 +54,7 @@ describe('Search Overlay', () => {
   it('Should not inject URL param after changing UI state', () => {
     cy.get('#button').click();
     cy.get('[data-testid="modal"]').find('[type="search"]').should('be.visible').type('jacket');
+    // Wait a bit for the URL updated due to debouncing effect
     // eslint-disable-next-line
     cy.wait(500);
     cy.url().should('not.include', 'q=jacket');
